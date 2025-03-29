e50349e3a868ba3e7953307c05a0a5755e5539cd
hys <243835440@qq.com>
2025-03-29 11:22:19 +0800

    feat(try add basic group entity): add IBasicGroupEntity

---
+++ src/Hx.Workflow.Domain/Hx/Workflow/Domain/IGroupBaseEntity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hx.Workflow.Domain
+{
+    /// <summary>
+    /// Group 基础类型接口
+    /// </summary>
+    /// <typeparam name="ItemEntity"></typeparam>
+    public interface IGroupBaseEntity<ItemEntity>
+    {
+        /// <summary>
+        /// 分组标题
+        /// </summary>
+        public string Title { get;}
+        /// <summary>
+        /// 路径枚举
+        /// </summary>
+        public string Code { get; }
+        /// <summary>
+        /// 分组排序
+        /// </summary>
+        public double Order { get; }
+        /// <summary>
+        /// 父Id
+        /// </summary>
+        public Guid? ParentId { get;}
+        /// <summary>
+        /// 分组描述
+        /// </summary>
+        public string? Description { get; }
+        /// <summary>
+        /// Get multi tenant id
+        /// </summary>
+        public Guid? TenantId { get;}
+        /// <summary>
+        /// 子组
+        /// </summary>
+        public List<IGroupBaseEntity<ItemEntity>> Children { get;}
+        /// <summary>
+        /// 表单
+        /// </summary>
+        public List<ItemEntity> Items { get; }
+    }
+}
