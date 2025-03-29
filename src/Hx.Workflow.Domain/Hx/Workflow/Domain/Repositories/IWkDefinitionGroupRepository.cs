331ddf75b1201045de2f1e4cc2e2c8c736fa5d17
hys <243835440@qq.com>
2025-03-29 23:43:40 +0800

    feat(definition group): modify management service

--- src/Hx.Workflow.Domain/Hx/Workflow/Domain/Repositories/IWkDefinitionGroupRepository.cs
+++ src/Hx.Workflow.Domain/Hx/Workflow/Domain/Repositories/IWkDefinitionGroupRepository.cs
@@ -6,7 +6,7 @@ using Volo.Abp.Domain.Repositories;
 
 namespace Hx.Workflow.Domain.Repositories
 {
-    public interface IWkDefinitionGroupRepository : IBasicRepository<WkDefinitionGroup, Guid>
+    public interface IWkDefinitionGroupRepository : IBasicRepository<WkDefinitionGroup, Guid>, IGroupRepository
     {
         /// <summary>
         /// 判断是否存在同一标题的组
@@ -20,18 +20,6 @@ namespace Hx.Workflow.Domain.Repositories
         /// <param name="id"></param>
         /// <returns></returns>
         Task<WkDefinitionGroup?> GetByIdAsync(Guid id);
-        /// <summary>
-        /// 获取某分类最大排序值
-        /// </summary>
-        /// <param name="parentId"></param>
-        /// <returns></returns>
-        Task<double> GetMaxOrderNumberAsync(Guid? parentId);
-        /// <summary>
-        /// 获取某分类下 Code 字段最后一个部分的最大值
-        /// </summary>
-        /// <param name="parentId">父分类ID</param>
-        /// <returns>最后一个部分的最大值对应的原始 Code，如果没有记录则返回 "00001"</returns>
-        Task<string> GetMaxCodeNumberAsync(Guid? parentId);
         /// <summary>
         /// 获取所有节点，包含子节点
         /// </summary>
